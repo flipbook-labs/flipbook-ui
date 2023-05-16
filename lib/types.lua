29a58b8b72bfb041e455c9e23aa14730e95354c7
pashleyy <64950564+passhley@users.noreply.github.com>
2023-05-15 22:15:44 -0400

    Deleted library wide type definitions

--- lib/types.lua
+++
@@ -1,8 +0,0 @@
-export type ReactBinding<T> = {
-	getValue: (self: ReactBinding<T>) -> any,
-	map: <U>(self: ReactBinding<T>, (any) -> any) -> any,
-}
-
-export type OptionalBinding<T> = T | ReactBinding<T>
-
-return nil
